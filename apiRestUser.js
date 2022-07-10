f1010af963ae2f79c13549af9d2ad9699724ee2f
JorgeHTcoding <jht.coding@gmail.com>
2022-07-10 19:37:54 +0200

    fixattemtp

---
+++ apiRestUser.js
@@ -0,0 +1,21 @@
+const { application } = require("express");
+
+let port = process.env.PORT || 3000;
+
+const connection = mysql.createConnection(
+    {
+        host        :"myshoulder.cdvg9i8dvcl6.eu-west-3.rds.amazonaws.com",
+        user        :"ComboWombo",
+        password    :"ComboWombo123!",
+        database    :"myshoulder"
+    });
+
+    connection.connect(function(error){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Conexión establecida")
+        }
+    });
+    
+    app.listen(port);
