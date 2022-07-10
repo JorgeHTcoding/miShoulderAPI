9c98f41496d9101f52e0fcc8fb36d19d69994224
JorgeHTcoding <jht.coding@gmail.com>
2022-07-10 19:55:17 +0200

    fixattempt9

--- apiRestUser.js
+++ apiRestUser.js
@@ -1,24 +1,45 @@
-const { application } = require("express");
+const express = require("express")
+const cors = require('cors')
+const errorHandling = require("./src/error/errorHandling")
+const userRouters = require("./routes/user.routers")
+const { app } = require("express");
 const mysql = require("mysql2");
-const app = require("../APImyShoulder/src/app")
 
+const app = express();
+app.set("port", process.env.PORT || 3000)
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(userRouters);
+;
+
+app.use(function (req, res, next) 
+    {
+        res.status(404).json({
+            error: true,
+            codigo: 404,
+            message: "Endpoint can't be found"
+        })
+    })
+
+app.use(errorHandling)
 
 let port = process.env.PORT || 3000;
 
 const connection = mysql.createConnection(
-    {
-        host        :"myshoulder.cdvg9i8dvcl6.eu-west-3.rds.amazonaws.com",
-        user        :"ComboWombo",
-        password    :"ComboWombo123!",
-        database    :"myshoulder"
-    });
+{
+    host        :"myshoulder.cdvg9i8dvcl6.eu-west-3.rds.amazonaws.com",
+    user        :"ComboWombo",
+    password    :"ComboWombo123!",
+    database    :"myshoulder"
+});
 
-    connection.connect(function(error){
-        if(error){
-            console.log(error);
-        }else{
-            console.log("Conexión establecida")
-        }
-    });
-
-    app.listen(port);
+connection.connect(function(error){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Conexión establecida")
+    }
+});
+app.listen(port);
+module.exports = connection;
