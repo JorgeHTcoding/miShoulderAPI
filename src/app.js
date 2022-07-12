745378891ac9b7d65e0eea02e6bcd06997a5b6f4
clau <claudia_diez@outlook.com>
2022-07-12 14:33:35 +0200

    actualizacionClau

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const cors = require('cors')
 const errorHandling = require("./error/errorHandling")
 const userRouters = require("./routes/user.routers")
+const adminPac = require("../src/routes/adminPac.routers")
+const busProfRouters = require("./routes/busProf.routers")
+
 
 const app = express();
 app.set("port", process.env.PORT || 3000)
@@ -9,6 +12,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(userRouters);
+app.use(adminPac);
+app.use(busProfRouters);
+
 ;
 
 app.use(function (req, res, next) 
